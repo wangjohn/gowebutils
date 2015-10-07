fa48333b04769feda5949aaa392b078bfd614340
wangjohn <wangjohn@mit.edu>
2015-10-07 10:27:49 -0700

    Added the PrepareRequestBody method.

---
+++ requests.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+const MaxRequestSize = 1048576 * 5
+
+func PrepareRequestBody(r *http.Request) ([]byte, error) {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, MaxRequestSize))
+	if err != nil {
+		return body, err
+	}
+	err = r.Body.Close()
+	return body, err
+}
