fc0194604c28f4f4706ca3ddc39eaef199b4920f
wangjohn <john.jian.wang@gmail.com>
2017-04-19 11:34:33 -0700

    Functions for sending multiple errors.

--- errors.go
+++ errors.go
@@ -15,9 +15,35 @@ func SendError(w http.ResponseWriter, err error) {
 		Detail: err.Error(),
 	}
 	errorList := []jsonapi.Error{e}
-	resp := jsonapi.ErrorResponse{
-		Errors: errorList,
+	sendErrorList(w, errorList)
+}
+
+func SendErrors(w http.ResponseWriter, errors []error) {
+	errorList := []jsonapi.Error{}
+	for _, e := range errors {
+		errorList = append(errorList, jsonapi.Error{
+			Status: "400",
+			Code:   "error",
+			Detail: e.Error(),
+		})
 	}
+	sendErrorList(w, errorList)
+}
+
+func SendErrorMessages(w http.ResponseWriter, errorMessages []string) {
+	errorList := []jsonapi.Error{}
+	for _, msg := range errorMessages {
+		errorList = append(errorList, jsonapi.Error{
+			Status: "400",
+			Code:   "error",
+			Detail: msg,
+		})
+	}
+	sendErrorList(w, errorList)
+}
+
+func sendErrorList(w http.ResponseWriter, errorList []jsonapi.Error) {
+	resp := jsonapi.ErrorResponse{Errors: errorList}
 	log.Printf("Returning error response: %+v", resp)
 	json.NewEncoder(w).Encode(resp)
 }
