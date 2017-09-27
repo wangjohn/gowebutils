b0cf394a358729674f7b366828483b4745925d45
wangjohn <john.jian.wang@gmail.com>
2017-09-26 20:39:30 -0700

    Add helper for unmarshaling the request body.

--- requests.go
+++ requests.go
@@ -1,6 +1,7 @@
 package gowebutils
 
 import (
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -15,4 +16,15 @@ func PrepareRequestBody(r *http.Request) ([]byte, error) {
 	}
 	err = r.Body.Close()
 	return body, err
+}
+
+func UnmarshalRequestBody(r *http.Request, v interface{}) error {
+	body, err := PrepareRequestBody(r)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(body, v); err != nil {
+		return err
+	}
+	return nil
 }
